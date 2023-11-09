41143d3b42a6a6cb4d5fad1e565f2a0787736aeb
isFY <fy77645@126.com>
2023-11-09 21:54:40 +0800

    恢复数据库

--- src/pages/Setting/DataBase/index.js
+++ src/pages/Setting/DataBase/index.js
@@ -1,61 +1,73 @@
 import React from "react";
 import {
   View,
+  ToastAndroid,
 } from 'react-native'
 import {List} from 'react-native-paper';
 import Header from "../../TagManage/Header";
 import * as Fs from 'expo-file-system'
-import Share from 'react-native-share'
+// import Share from 'react-native-share'
 import {shareAsync} from 'expo-sharing'
 import {pickDirectory} from 'react-native-document-picker'
 import {getDocumentAsync} from 'expo-document-picker'
 
-export default function DataBase({navigation}){
-  
-  const lookSqdir = async ()=>{
-    const path = Fs.documentDirectory + 'SQLite'
-    const sqDir = await Fs.getInfoAsync(path)
-    if(sqDir.exists){
-        const files = await Fs.readDirectoryAsync(path)
-        console.log({files});
-    }else{
-        console.log('SQLite 文件不存在');
-    }
+const lookSqdir = async ()=>{
+  const path = Fs.documentDirectory + 'SQLite'
+  const sqDir = await Fs.getInfoAsync(path)
+  if(sqDir.exists){
+      const files = await Fs.readDirectoryAsync(path)
+      console.log({files});
+      const file = await Fs.getInfoAsync(path + '/db.db')
+      console.log({file});
+  }else{
+      console.log('SQLite 文件不存在');
   }
-  const shareDataBase = async ()=>{
-    const url = Fs.documentDirectory + 'SQLite/db.db'
-    const file = await Fs.getInfoAsync(url)
-    console.log({file});
-    // shareAsync(url,{dialogTitle:'dialogTitle'})
-  }
-  /* 
-  
-  SQLite数据库：application/vnd.sqlite3 或 application/x-sqlite3
+}
+
+const shareDataBase = async ()=>{
+  const url = Fs.documentDirectory + 'SQLite/db.db'
+  shareAsync(url,{dialogTitle:'dialogTitle'})
+}
+
+/* 
+SQLite数据库：application/vnd.sqlite3 或 application/x-sqlite3
 MySQL数据库：application/x-mysql
 PostgreSQL数据库：application/x-postgresql
-  */
+*/
+const backupDataBase = async ()=>{
+  let cancled = false
+  const res = await pickDirectory().catch(()=>{
+    cancled = true
+  })
+  console.log({res});
+  if(cancled) return
+  const uri = await Fs.StorageAccessFramework.createFileAsync(res.uri,'myPassword.db','application/vnd.sqlite3')
+  console.log({uri});
+  const from = Fs.documentDirectory + 'SQLite/db.db'
+  const content = await Fs.readAsStringAsync(from,{
+    encoding:Fs.EncodingType.Base64
+  })
+  await Fs.StorageAccessFramework.writeAsStringAsync(uri,content,{
+    encoding:Fs.EncodingType.Base64
+  })
+  ToastAndroid.show('备份成功',ToastAndroid.SHORT)
+}
 
-  const backupDataBase = async ()=>{
-    const res = await pickDirectory()
-    console.log({res});
-    
-    // await Fs.StorageAccessFramework.createFileAsync(res.uri,'db',)
-    // const res2 = await Fs.StorageAccessFramework.requestDirectoryPermissionsAsync()
-    // console.log({res2});
-    // const url = await Fs.getContentUriAsync(res.uri)
-    // console.log({url});
-    const from = Fs.documentDirectory + 'SQLite/db.db'
-    const to = res.uri + '/db.db'
-    // await Fs.StorageAccessFramework.copyAsync({
-    //   from,
-    //   to,
-    // })
-    // const content = await Fs.readAsStringAsync(from)
-    // await Fs.StorageAccessFramework.writeAsStringAsync(to,content)
-    // console.log('ok');
-    const uri = await Fs.StorageAccessFramework.createFileAsync(res.uri,'db','application/x-sqlite3')
-    console.log({uri});
-  }
+const restoreDataBase = async ()=>{
+  const res = await getDocumentAsync()
+  if(res.canceled) return
+  const file = res.assets[0]
+  console.log({file});
+  if(!file.name.endsWith('.db')) return ToastAndroid.show('文件类型错误',ToastAndroid.SHORT)
+  await Fs.copyAsync({
+    from:file.uri,
+    to:Fs.documentDirectory + 'SQLite/db.db'
+  })
+  ToastAndroid.show('恢复成功',ToastAndroid.SHORT)
+}
+
+export default function DataBase({navigation}){
+  
 
   return (
     <View
@@ -76,7 +88,8 @@ PostgreSQL数据库：application/x-postgresql
           }}></List.Item>
           <List.Item onPress={shareDataBase} title="共享数据库" description="创建并共享一个加密数据库的备份"></List.Item>
           <List.Item onPress={backupDataBase} title="备份数据库" description="创建加密数据库的备份"></List.Item>
-          <List.Item title="恢复数据库" description="从加密数据库恢复数据库"></List.Item>
+          <List.Item onPress={restoreDataBase} title="恢复数据库" description="从加密数据库恢复数据库"></List.Item>
+          {/* <List.Item onPress={lookSqdir} title="查看目录"></List.Item> */}
         </View>
       </View>
     </View>
